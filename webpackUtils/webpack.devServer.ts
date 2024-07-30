cc61d7d2c85ecb1dc9ac58ae564bec29e3178731
Prokopchuk-Valentin <valentin.p@rocketfirm.tech>
2024-07-30 15:41:36 +0500

    добавляет dev serv

---
+++ webpackUtils/webpack.devServer.ts
@@ -0,0 +1,10 @@
+/** @format */
+
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+
+const devServer: DevServerConfiguration = {
+  port: 5050,
+  open: true,
+};
+
+export default devServer;
